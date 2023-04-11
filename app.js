ea59a6e1f1b0c325327368ef19e2ee8e18e2bb9a
HYHYJ <jjangdaram@naver.com>
2023-04-12 07:51:28 +0900

    2023.04.12-3

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const canvas = document.querySelector("canvas");
 
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d"); //getContext는 붓이다.
+canvas.width = 800;
+canvas.height = 800; //css랑 javascript에 둘다 쓰기.
+
+
+
+ctx.fillRect(50, 50, 100, 200)
